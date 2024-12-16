ad50aade1ad2552e1ddaa36f89570e0f6a390e90
Maneet Goyal <maneetgoyal14@gmail.com>
2024-12-16 18:11:41 +0530

    refactor: remove incomplete

--- src/renderer/components/CreatableSelect/index.stories.tsx
+++
@@ -1,9 +0,0 @@
-import { CreatableSelect } from "../CreatableSelect";
-import type { Meta, StoryObj } from "@storybook/react";
-
-export default {
-  title: "Components/CreatableSelect",
-  component: CreatableSelect,
-} as Meta<typeof CreatableSelect>;
-
-export const Template: StoryObj = {};
